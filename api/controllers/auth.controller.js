df8d4fd285bf6bfac06a3318c0bb24894d213d78
Mike <mike2021us@gmail.com>
2023-10-11 10:56:21 -0700

    complete sign up page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,6 +4,7 @@ import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+  console.log(req.body);
   const hashedPwd = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPwd });
   try {
