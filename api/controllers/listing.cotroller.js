9b35998a9a1ab640138bfcdb326fc07f7d04196a
Mike <mike2021us@gmail.com>
2023-10-14 22:26:59 -0700

    create get user listing api route

--- api/controllers/listing.cotroller.js
+++ api/controllers/listing.cotroller.js
@@ -1,9 +1,22 @@
-import Listing from '../models/listing.model.js'
+import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
-    return res.status(201).json(listing);
+    return res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserListing = async (req, res, next) => {
+  console.log(req);
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only view your own listings!"));
+  try {
+    const listing = await Listing.find({ userRef: req.params.id });
+    return res.status(200).json(listing);
   } catch (error) {
     next(error);
   }
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createListing } from "../controllers/listing.cotroller.js";
-import { verifyToken } from "../utils/verifyUser.js";
+import { createListing, getUserListing } from "../controllers/listing.cotroller.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
+router.get("/user_listing/:id", verifyToken, getUserListing);
 
 export default router;
